b04cd016f227687b22ad6ad89470d1d37e766f46
Florentine Ramboeck <florentine.ramboeck@web.de>
2025-02-07 16:23:19 +0100

    Add ESLint Config

--- app/products/page.js
+++ app/products/page.js
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Plantify',
+  description: 'Find beautiful plants online',
+};
+
 export default function ProductsPage() {
   const products = getProducts();
-  return;
-  <div>
-    <h1></h1>
-  </div>;
+  return (
+    <div>
+      <h1>My plants</h1>
+      {products.map((product) => {
+        return (
+          <div key={`product-${product.id}`}>
+            <Link href={`/products/${product.id}`}>
+              <div>{product.name}</div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
